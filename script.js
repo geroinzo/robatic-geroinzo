3d222a0b0d60bc7223b4d3db2b1bcb7bfce668d1
geroinzo <83762086+geroinzo@users.noreply.github.com>
2025-03-06 21:40:13 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
-document.getElementById('generate').addEventListener('click', generatePoster);
+@font-face {
+    font-family: 'ZI_SQUARE';
+    src: url('assets/fonts/ZI_SQUARE.ttf') format('truetype');
+}
 
-function generatePoster() {
-    const canvas = document.getElementById('posterCanvas');
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-    const bgIndex = Math.floor(Math.random() * 5) + 1;
-    const imgIndex = Math.floor(Math.random() * 16) + 1;
-    const textIndex = Math.floor(Math.random() * 10) + 1;
-    
-    const bg = new Image();
-    bg.src = `assets/backgrounds/${bgIndex}.png`;
-    bg.onload = () => {
-        ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
-        
-        const img = new Image();
-        img.src = `assets/image/${imgIndex}.png`;
-        img.onload = () => {
-            ctx.drawImage(img, 225, 480, 1080, 1080);
-            
-            fetch(`assets/text/${textIndex}.txt`)
-                .then(response => response.text())
-                .then(text => {
-                    ctx.fillStyle = 'black';
-                    ctx.font = '40px "Press Start 2P"';
-                    ctx.fillText(text, 100, 200);
-                });
-        };
-    };
+body {
+    background-color: black;
+    color: white;
+    font-family: 'ZI_SQUARE', sans-serif;
+    text-align: center;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+}
+
+button {
+    font-size: 16px;
+    padding: 10px 20px;
+    margin: 10px;
+    border: 2px solid white;
+    background: black;
+    color: white;
+    cursor: pointer;
+    font-family: 'ZI_SQUARE', sans-serif;
+}
+
+.image-container {
+    position: relative;
+    width: 500px;
+    height: 500px;
+    overflow: hidden; /* Обрезка всего, что выходит за границы */
+    border: 4px solid white;
+}
+
+canvas {
+    max-width: 100%;
+    height: auto;
 }
